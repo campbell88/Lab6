eeb76b79267e3ba8709877998e3439749958f437
campbell88 <shatharcampbell@gmail.com>
2017-04-17 15:34:31 -0400

    added methods to clean up code

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -1,45 +1,75 @@
-﻿using System;
+﻿// Author: Shathar Campbell
+// 04/17/17
+// Pig Latin Lab
+
+//TO DO:
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lab6
-{
+{ //Updated lab to include new methods
     class Program
     {
         static void Main(string[] args)
         {
 
+
             do
             {
                 Console.WriteLine("Welcome to the Pig Latin Translator!\n");
-                Console.WriteLine("Enter a line to be translated: \n");
-                string word = Console.ReadLine().ToLower();
-                //created an indexofany to show the number of characters a word has before the first vowel in said word
-                char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
-                int index = word.IndexOfAny(vowels);
-
-                //check if word starts with vowel
-                if ((word.StartsWith("a")) || (word.StartsWith("e")) || (word.StartsWith("i")) || (word.StartsWith("o")) || (word.StartsWith("u")))
-                {
-                    Console.WriteLine(word + "way");
-                }
-
-                else //if (index == -1)  
-                {
-                    string removedConsonants = word.Substring(index);
-                    string con = (word.Substring(0, index));
-                    Console.WriteLine(removedConsonants + con + "ay\n");
-                }
-
-                Console.WriteLine("Do you wish to continue?:  ");
+                Console.WriteLine("Enter a word to be translated: \n");
+                NewWord();
+                Console.WriteLine("Type yes to continue:  ");
 
             } while (Console.ReadLine().ToUpper() == "YES");
         }
 
 
+        //method to translate word
+        public static void NewWord()
+        {
+            string word = Console.ReadLine().ToLower();
+            word = CheckifWord(word);// checks if word is a valid entry (not numbers, characters, etc see method below) 
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            int index = word.IndexOfAny(vowels);
+
+            //check if word starts with vowel - make if else statement a method
+            if ((word.StartsWith("a")) || (word.StartsWith("e")) || (word.StartsWith("i")) || (word.StartsWith("o")) || (word.StartsWith("u")))
+            {
+                Console.WriteLine(word + "way");
+            }
+
+
+            else //if (index == -1)  
+            {
+                string removedConsonants = word.Substring(index);
+                string con = (word.Substring(0, index));
+                Console.WriteLine(removedConsonants + con + "ay\n");
+
+
+            }
+        }
+
+        public static string CheckifWord(string word)
+        {
+
+
+            while (!Regex.IsMatch(word, @"[A-Za-z]+$"))
+            {
+                Console.WriteLine("Invalid entry: please enter a word:  ");
+                word = Console.ReadLine();
+            }
+            return word;
+        }
 
     }
 }
+
+
 
